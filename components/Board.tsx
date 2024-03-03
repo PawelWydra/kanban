35db17734b13ef26e8376d8609754dda6030c579
pawel <wydrapawel@icloud.com>
2024-03-03 08:23:47 +0100

    chow: add colors to css var

--- components/Board.tsx
+++ components/Board.tsx
@@ -5,7 +5,7 @@ function Board() {
     <>
       <SideBar />
       <div className="w-10/12 h-full flex flex-col gap-6 justify-center items-center">
-        <h3 className="text-gray-400">
+        <h3 className="text-md ">
           This board is empty. Create a new column to get started
         </h3>
         <button className="h-12 w-48 bg-violet-600 rounded-3xl heading-s text-white p-1">
