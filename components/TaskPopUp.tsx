8149b674fe8696df675ae137078529f0198f7e01
pawel <wydrapawel@icloud.com>
2024-03-04 14:23:51 +0100

    feat: popup window for task

---
+++ components/TaskPopUp.tsx
@@ -0,0 +1,23 @@
+
+
+const TaskPopUp = () => {
+  return (
+    <div className="flex justify-center items-center bg-black opacity-50">
+      <div className="flex flex-col items-center gap-4 p-4">
+        <h1 className="heading-lg"></h1>
+        <p className="text-body-md text-gray-medium"></p>
+        <div>
+          <p className="heading-sm text-gray-medium">Subtask (2 of 3)</p>
+          <div className="bg-gray-light">
+            <input type="checkbox" id="task1" name="task1" />
+            <label className="text-body-md" htmlFor="task1">
+              I have a bike
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskPopUp;
