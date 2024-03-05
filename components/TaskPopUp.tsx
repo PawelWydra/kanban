243d12d2202112f658da88b652e0f8e59eae21b6
pawel <wydrapawel@icloud.com>
2024-03-05 11:26:03 +0100

    feat: add TaskPopUp design

--- components/TaskPopUp.tsx
+++ components/TaskPopUp.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const TaskPopUp = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxClick = () => {
+    setIsChecked((prevState) => !prevState);
+    console.log("Checkbox");
+  };
+
   return (
-    <div className="absolute h-screen w-screen bg-gray-900/60 flex justify-center items-center ">
-      <div className="bg-white w-[30rem] h-[32.7rem] flex flex-col items-center gap-4 p-4">
+    <div className="absolute h-screen w-screen bg-gray-900/60 flex justify-center items-center">
+      <div className="bg-white w-[30rem] h-[32.7rem] flex flex-col gap-4 p-8 rounded-2xl">
         <h1 className="heading-lg">
           Research pricing points of various competitors and trial different
           business models
@@ -13,15 +28,32 @@ const TaskPopUp = () => {
           finalise the first pricing model we'll use. Keep iterating the
           subtasks until we have a coherent proposition.
         </p>
-        <p className="heading-sm text-gray-medium">Subtask (2 of 3)</p>
 
-        <div className="w-full rounded-xl  py-2">
+        <div className="w-full rounded-xl py-2">
+          <p className="text-body-md text-gray-medium mb-4">Subtask (2 of 3)</p>
           <div className="flex items-center gap-4 h-12 px-4 bg-gray-light rounded-lg">
-            <Checkbox />
-            <label className="text-body-md" htmlFor="task1">
+            <Checkbox onCheckedChange={handleCheckboxClick} />
+            <label
+              className={`text-body-md ${
+                isChecked ? "opacity-60 line-through" : ""
+              }`}
+            >
               Research competitor pricing and business models
             </label>
           </div>
+        </div>
+        <div className="flex flex-col gap-1">
+          <p className="text-body-md text-gray-medium">Current Status</p>
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Doing" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="light">To do</SelectItem>
+              <SelectItem value="dark">Doing</SelectItem>
+              <SelectItem value="system">Done</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
       </div>
     </div>
