5c86835eaa1b7c29cd5b9b2a5c87985777a71101
pawel <wydrapawel@icloud.com>
2024-03-14 13:54:41 +0100

    feat: add modals to ModalContainer

--- components/ModalContainer.tsx
+++ components/ModalContainer.tsx
@@ -1,8 +1,25 @@
 "use client"
 
+import { useState } from "react"
+import AddNewBoard from "./modals/AddNewBoard"
+import AddNewTask from "./modals/AddNewTask"
+import DeleteWarning from "./modals/DeleteWarning"
+import EditTask from "./modals/EditTask"
+import NewColumn from "./modals/NewColumn"
+import TaskInfo from "./modals/TaskInfo"
+
+
 const ModalContainer = () => {
   return (
-    <div>ModalContainer</div>
+    <>
+    <AddNewBoard/>
+    <AddNewTask/>
+    <DeleteWarning/>
+    <EditBoard/>
+    <EditTask/>
+    <NewColumn/>
+    <TaskInfo/>
+    </>
   )
 }
 
