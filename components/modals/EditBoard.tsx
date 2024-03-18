bee613932a5914aee60d3dac1cff56a36bf09689
pawel <wydrapawel@icloud.com>
2024-03-18 19:44:57 +0100

    feat(EditBoard): add and manage states

--- components/modals/EditBoard.tsx
+++ components/modals/EditBoard.tsx
@@ -1,11 +1,68 @@
-"use client";
-
-import Image from "next/image";
-import Cross from "@/assets/icon-cross.svg";
 import useEscape from "../helpers/useEscapeFunction";
+import { useState, ChangeEvent, useContext, useEffect } from "react";
+import DataInput from "./datainputs/DataInput";
+import DeleteInputButton from "./datainputs/DeleteInputButton";
+import { HomeContext } from "@/context/HomeContext";
 
 function EditBoard() {
   const isVisible: boolean = useEscape();
+  const useHomeStateContext = () => useContext(HomeContext);
+  const { boardSelectedId, boards } = useHomeStateContext();
+  let completeBoardSelected = boards.find(
+    (board) => board.id === boardSelectedId
+  );
+  const [currentBoard, setCurretnedBoard] = useState(completeBoardSelected);
+
+  const handleColumnNameChange = (index: number, value: string) => {
+    const updatedColumns = [...currentBoard.columns];
+    updatedColumns[index].name = value;
+    setCurretnedBoard((prev) => ({
+      ...prev,
+      columns: updatedColumns,
+    }));
+  };
+  const handleDeleteColumn = (index: number) => {
+    if (!currentBoard) return;
+    const updatedColumns = currentBoard.columns.filter(
+      (column, columnIndex) => columnIndex !== index
+    );
+    setCurretnedBoard((prev) => ({
+      ...prev,
+      columns: updatedColumns,
+    }));
+  };
+
+  const handleAddColumn = () => {
+    if (!currentBoard) return;
+    setCurretnedBoard((prev) => ({
+      ...prev,
+      columns: [...prev.columns, { name: "" }],
+    }));
+  };
+
+  const EditBoard = async () => {
+    try {
+      const response = await fetch("your-api-url", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(currentBoard),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to update board");
+      }
+
+      const data = await response.json();
+
+      // Provide feedback to the client
+      alert("Board updated successfully");
+    } catch (error) {
+      // Provide feedback to the client
+      alert("Failed to update board. Please try again later.");
+    }
+  };
 
   return (
     isVisible && (
@@ -13,60 +70,42 @@ function EditBoard() {
         <div className="bg-white w-[30rem] flex flex-col gap-4 p-8 rounded-2xl">
           <h1 className="heading-lg">Edit Board</h1>
           <div className="flex flex-col mt-2">
-            <label className="text-body-md text-gray-medium">Board Name</label>
+            <label className="text-body-md text-gray-medium">Name</label>
             <input
               className="ring-2 p-2 ring-gray-light rounded"
               type="text"
               placeholder="e.g. Web Design"
+              value={currentBoard?.name}
             />
           </div>
           <div className="flex flex-col gap-4">
-            <h2 className="text-body-md text-gray-medium">Board Columns</h2>
-            <div className="flex items-center">
-              <input
-                className="w-11/12 p-2 ring-2 ring-gray-light rounded"
-                type="text"
-                placeholder="Todo"
-              />
-              <Image className="ml-auto" src={Cross} alt="delete subtask" />
-            </div>
-            <div className="flex items-center gap-2">
-              <input
-                className="w-11/12 p-2 ring-2 ring-gray-light rounded"
-                type="text"
-                placeholder="Doing"
-              />
-              <Image
-                className="ml-auto hover:cursor-pointer"
-                src={Cross}
-                alt="delete subtask"
-              />
-            </div>
-            <div className="flex items-center gap-2">
-              <input
-                className="w-11/12 p-2 ring-2 ring-gray-light rounded"
-                type="text"
-                placeholder="Done"
-              />
-              <Image
-                className="ml-auto hover:cursor-pointer"
-                src={Cross}
-                alt="delete subtask"
-              />
-            </div>
-            <button className="h-10 bg-gray-light hover:bg-gray-100 text-purple text-body-md rounded-3xl">
+            <h2 className="text-body-md text-gray-medium">Columns</h2>
+            {currentBoard?.columns.map((column, index) => (
+              <div className="flex items-center gap-4" key={index}>
+                <DataInput
+                  value={column.name}
+                  onChange={(value) => handleColumnNameChange(index, value)}
+                />
+                <DeleteInputButton onClick={() => handleDeleteColumn(index)} />
+              </div>
+            ))}
+            <button
+              className="h-10 bg-gray-light hover:bg-gray-100 text-purple text-body-md rounded-3xl"
+              onClick={handleAddColumn}
+            >
               + Add New Column
             </button>
           </div>
-          <button className="h-10 bg-purple hover:bg-purple-hover text-white text-body-md rounded-3xl">
-            Save Ch
+          <button
+            className="h-10 bg-purple hover:bg-purple-hover text-white text-body-md rounded-3xl"
+            onClick={EditBoard}
+          >
+            Save Changes
           </button>
         </div>
       </div>
     )
   );
 }
-
-EditBoard.propTypes = {};
 
 export default EditBoard;
