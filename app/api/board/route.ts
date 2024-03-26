e33170afccd7228eb8bbe208d83117c3cd3ca1f2
pawel <wydrapawel@icloud.com>
2024-03-26 19:27:21 +0100

    api: add delete route for board

--- app/api/board/route.ts
+++ app/api/board/route.ts
@@ -1,80 +1,13 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/lib/prisma";
-import { editColumnViaBoard } from "../column/route";
+import { NextResponse } from "next/server";
 
-export const getBoardById = async (id: string) => {
-  const board = await prisma.board.findMany({
+export async function DELETE(request: Request) {
+  const body = await request.json();
+  const id = body.id;
+  await prisma.board.delete({
     where: {
-      id,
-    },
-    include: {
-      columns: {
-        include: {
-          tasks: true,
-        },
-      },
+      id: id,
     },
   });
-  return board;
-};
-
-export const createBoard = async (reqBody: any) => {
-  const newBoard = await prisma.board.create({
-    data: {
-      name: reqBody.name,
-      columns: {
-        create: reqBody.columns.map((column: any) => ({
-          name: column.name,
-        })),
-      },
-    },
-  });
-  const board = await getBoardById(newBoard.id);
-  return board;
-};
-
-export const editBoard = async (reqBody: any, id: string) => {
-  const updateBoard = await prisma.board.update({
-    where: {
-      id: reqBody.id,
-    },
-    data: {
-      name: reqBody.name,
-    },
-  });
-  const board = await getBoardById(reqBody.id);
-  return board;
-};
-
-export const deleteBoard = async (boardId: string) => {
-  const deleteBoard = await prisma.board.delete({
-    where: {
-      id: boardId,
-    },
-  });
-  return deleteBoard;
-};
-
-export default async function handle(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  try {
-    switch (req.method) {
-      case "POST":
-        const newBoard = await createBoard(req.body);
-        return res.json(newBoard);
-      case "PUT":
-        const updateBoard = await editBoard(req.body, req.body.id);
-        const updateColumns = await editColumnViaBoard(req.body);
-        return res.json({ updateBoard, updateColumns });
-      case "DELETE":
-        const deletedBoard = await deleteBoard(req.body.boardId);
-        return res.json(deletedBoard);
-      default:
-        break;
-    }
-  } catch (error: any) {
-    return res.status(500).json({ message: error.message });
-  }
+  return NextResponse.json("Board deleted successfully!", { status: 200 });
 }
