f853ee80641fad29b3160dfc02f815867dcf3a43
pawel <wydrapawel@icloud.com>
2024-03-26 20:48:03 +0100

    refactor: add focus and blur handle to error message

--- components/modals/datainputs/DataInput.tsx
+++ components/modals/datainputs/DataInput.tsx
@@ -1,22 +1,35 @@
+import { useState } from "react";
+
 type DataInputProps = {
   value: string;
   onChange: (value: string) => void;
+  onBlur: () => void;
   error?: boolean;
 };
 
-function DataInput({ value, onChange, error = false }: DataInputProps) {
+function DataInput({ value, onChange, onBlur, error = false }: DataInputProps) {
+  const [hasFocus, setHasFocus] = useState(false);
+
+  const handleBlur = () => {
+    setHasFocus(true);
+  };
+
   return (
-    <div className={`relative w-11/12 p-2 ring-2 ${
-      error ? "ring-destructive" : "ring-gray-light"
-    } rounded`}>
+    <div
+      className={`relative w-11/12 p-2 ring-2 ${
+        error && hasFocus ? "ring-destructive" : "ring-gray-light"
+      } rounded`}
+    >
       <input
-      className="w-full h-full bg-transparent outline-none"
+        className="h-full w-full outline-none"
         type="text"
         placeholder="name..."
         value={value}
         onChange={(event) => onChange(event.target.value)}
+        onBlur={handleBlur}
+        onFocus={() => setHasFocus(false)}
       />
-      {error && (
+      {error && hasFocus && (
         <span className="absolute right-2 top-1/2 transform -translate-y-1/2 text-destructive text-sm">
           Can&apos;t be empty
         </span>
