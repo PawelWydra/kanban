d2ce5be9649c77e1a21d7d855ab92daece3ba4a5
pawel <wydrapawel@icloud.com>
2024-04-17 19:50:28 +0200

    style: delete export from getBoards

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import SideBar from "@/components/Sidebar";
 import Logo from "@/components/Logo";
 import prisma from "@/lib/prisma";
 import ModalContainer from "@/components/ModalContainer";
-import { IBoard } from "@/types"; 
+import { IBoard } from "@/types";
 
-export const getBoards = async () => {
+const getBoards = async () => {
   const fetchedBoards = await prisma.board.findMany({
     include: {
       columns: {
@@ -17,7 +17,6 @@ export const getBoards = async () => {
   });
   return fetchedBoards;
 };
-
 
 export default async function Home() {
   const boards: IBoard[] = await getBoards();
